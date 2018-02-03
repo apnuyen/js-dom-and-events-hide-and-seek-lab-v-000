2a15c17aa3de7d85a22a2df694a30067a673e7f2
An Nuyen <annuyen@gmail.com>
2018-02-03 15:48:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-function getFirstSelector(selector){
+function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
-function nestedTarget(){
+function nestedTarget() {
   return document.querySelector('div#nested div.target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML, 10) + n
+  }
 }
